c7867354315226524f1871a41c66bde3d0ea8923
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-09-08 21:17:58 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,26 +1,26 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def find_ticket_price(age):
+def check_number(num):
     # Write your code here
     pass
 
 # Exercise 2
-def is_four_digit_number(number):
+def rectangle_area(length, width):
     # Write your code here
     pass
 
 # Exercise 3
-def is_divisible(num1, num2):
+def is_divisible_by_5(num):
     # Write your code here
     pass
 
 # Exercise 4
-def sum_of_even_numbers(num1, num2, num3):
+def sum_is_even_or_odd(a, b, c, d):
     # Write your code here
     pass
 
 # Exercise 5
-def convert_time_to_days_hours(hours):
+def first_last_equal(num):
     # Write your code here
     pass

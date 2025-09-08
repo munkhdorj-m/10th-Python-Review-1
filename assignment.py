320003cd67d44ef0fd571e22e929769c74416bf8
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-09-08 21:36:15 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,12 +1,12 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def check_number(num):
+def rectangle_area(length, width):
     # Write your code here
     pass
 
 # Exercise 2
-def rectangle_area(length, width):
+def check_number(num):
     # Write your code here
     pass
 
